53b046edc5aecc815d2e59b78940bca3e9f5f0e6
David Setiawan <setdavid1014@gmail.com>
2021-11-14 04:16:27 -0500

    added hammerjs

--- client/src/Components/SwipeCard.jsx
+++ client/src/Components/SwipeCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Hammer from "react-hammerjs";
 
 function SwipeCard() {
     let swipeCardCSS = {
@@ -7,17 +8,17 @@ function SwipeCard() {
         height: "75vh"
     }
 
-    const handleMouseDown = (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        console.log("mousedown")
+    const handleSwipe = (e) => {
+        console.log(e)
     }
 
     return (
-        <div id="swipe-card" style={swipeCardCSS}
-            onMouseDown={e => handleMouseDown(e)}>
-
-        </div>
+        // <div id="swipe-card" style={swipeCardCSS}>
+        //     <Hammer onTap={handleTap}><div>Tap Me</div></Hammer>
+        // </div>
+        <Hammer className="swipe-card" style={swipeCardCSS} onSwipe={handleSwipe}>
+            <div>Tap Me</div>
+        </Hammer>
     );
 }
 
