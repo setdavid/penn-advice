cbbd00eb457f75eb2e0057017caf1bdde6dc85e9
David Setiawan <setdavid1014@gmail.com>
2021-12-29 23:50:44 -0500

    improved navbar experience

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faClone, faUserNinja } from "@fortawesome/free-solid-svg-icons"
 import { setDisplay, SWIPE_CONTENT, PERSONAL_CONTENT } from "../redux/ducks/mobile";
 
-export let NAV_BAR_HEIGHT = 50;
+export let NAV_BAR_HEIGHT = 60;
 export let MOBILE_DISPLAY_TRANSITION_DURATION = 250
 
 function NavBar() {
@@ -30,11 +30,11 @@ function NavBar() {
     return (
         <div id="nav-bar" className="container-fluid" style={navbarCSS}>
             <div className="row full-height">
-                <div className="col-6 full-height nav-bar-btn">
-                    <FontAwesomeIcon icon={faClone} size="lg" style={display == SWIPE_CONTENT ? displayBtnCSS : notDisplayBtnCSS} onClick={() => dispatch(setDisplay(SWIPE_CONTENT))} />
+                <div className="col-6 full-height nav-bar-btn" onClick={() => dispatch(setDisplay(SWIPE_CONTENT))}>
+                    <FontAwesomeIcon icon={faClone} size="lg" style={display == SWIPE_CONTENT ? displayBtnCSS : notDisplayBtnCSS} />
                 </div>
-                <div className="col-6 full-height nav-bar-btn">
-                    <FontAwesomeIcon icon={faUserNinja} size="lg" style={display == PERSONAL_CONTENT ? displayBtnCSS : notDisplayBtnCSS} onClick={() => dispatch(setDisplay(PERSONAL_CONTENT))} />
+                <div className="col-6 full-height nav-bar-btn" onClick={() => dispatch(setDisplay(PERSONAL_CONTENT))}>
+                    <FontAwesomeIcon icon={faUserNinja} size="lg" style={display == PERSONAL_CONTENT ? displayBtnCSS : notDisplayBtnCSS} />
                 </div>
             </div>
         </div>
