2b1ed9d7a775ce29c9354faf29cd248ce39d0df0
David Setiawan <setdavid1014@gmail.com>
2021-12-31 21:55:30 -0500

    started account content

--- client/src/Components/AccountContent.jsx
+++ client/src/Components/AccountContent.jsx
@@ -1,3 +1,5 @@
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 function AccountContent(props) {
@@ -6,7 +8,16 @@ function AccountContent(props) {
 
     return (
         <div id="account-content" className="full-height" style={{ ...style, ...accountContentCSS }}>
-            Account Content
+            <div className="row">
+                <div className="col-12 d-flex justify-content-center" style={{ marginTop: "5vh" }}>
+                    <FontAwesomeIcon icon={faUserSecret} size="6x" />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12 d-flex justify-content-center" style={{ marginTop: "10px", fontSize: "1.5rem", fontWeight: "bold" }}>
+                    User #12345
+                </div>
+            </div>
         </div >
     );
 }
